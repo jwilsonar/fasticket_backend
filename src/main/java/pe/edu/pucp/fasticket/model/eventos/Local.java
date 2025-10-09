a149340b3673a3cc4363c937cf50415a8164ab2e
jwilsonar <wilsonalbornozdev@gmail.com>
2025-10-09 16:43:09 -0500

    Correción para test; código duplicado

--- src/main/java/pe/edu/pucp/fasticket/model/eventos/Local.java
+++ src/main/java/pe/edu/pucp/fasticket/model/eventos/Local.java
@@ -2,6 +2,7 @@ package pe.edu.pucp.fasticket.model.eventos;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -9,7 +10,10 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pe.edu.pucp.fasticket.model.geografia.Distrito;
 
 @Data
 @NoArgsConstructor
@@ -17,12 +21,10 @@ import lombok.NoArgsConstructor;
 @ToString(exclude = {"distrito"})
 @Entity
 @Table(name = "Local")
-@Table(name = "Local")
 public class Local {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "idLocal")
     @Column(name = "idLocal")
     private Integer idLocal;
 
@@ -33,25 +35,20 @@ public class Local {
     private String direccion;
 
     @Column(name = "aforoTotal")
-    @Column(name = "aforoTotal")
     private Integer aforoTotal;
 
     @Column(name = "activo")
     private Boolean activo = true;
 
     @Column(name = "usuarioCreacion")
-    @Column(name = "usuarioCreacion")
     private Integer usuarioCreacion;
 
-    @Column(name = "fechaCreacion")
     @Column(name = "fechaCreacion")
     private java.time.LocalDate fechaCreacion;
 
     @Column(name = "usuarioActualizacion")
-    @Column(name = "usuarioActualizacion")
     private Integer usuarioActualizacion;
 
-    @Column(name = "fechaActualizacion")
     @Column(name = "fechaActualizacion")
     private java.time.LocalDate fechaActualizacion;
 

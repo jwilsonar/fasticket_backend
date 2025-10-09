cd05f23db276867b134698becbccc485b8164d90
jwilsonar <wilsonalbornozdev@gmail.com>
2025-10-09 03:40:34 -0500

    Correcciones en test para despliegue

--- src/test/java/pe/edu/pucp/fasticket/FasticketAppApplicationTests.java
+++ src/test/java/pe/edu/pucp/fasticket/FasticketAppApplicationTests.java
@@ -2,8 +2,10 @@ package pe.edu.pucp.fasticket;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FasticketAppApplicationTests {
 
 	@Test

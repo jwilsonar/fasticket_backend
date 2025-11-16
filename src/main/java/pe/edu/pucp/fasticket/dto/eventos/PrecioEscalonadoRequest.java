03174a5d32e514efa3d1d9093d84f08d64c01863
FelixElCoder <felix14life2020@gmail.com>
2025-11-16 02:49:45 -0500

    -Cambios épicos
    Para solucionar los test

--- src/main/java/pe/edu/pucp/fasticket/dto/eventos/PrecioEscalonadoRequest.java
+++ src/main/java/pe/edu/pucp/fasticket/dto/eventos/PrecioEscalonadoRequest.java
@@ -4,13 +4,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import pe.edu.pucp.fasticket.model.eventos.Etapa;
 import pe.edu.pucp.fasticket.model.eventos.TipoTicket;
 
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(description = "DTO para crear PrecioEscalonado")
 public class PrecioEscalonadoRequest {
 
--- src/main/java/pe/edu/pucp/fasticket/dto/eventos/TipoTicketRequest.java
+++ src/main/java/pe/edu/pucp/fasticket/dto/eventos/TipoTicketRequest.java
@@ -5,11 +5,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(description = "Verdadero DTO para crear TipoTicket")
 public class TipoTicketRequest {
     @Schema(description = "ID del TipoTicket", example = "1")
